3ec8d49233cc5f1549713bb601d83adef1497aae
Jamie Isaacs <pdt256@gmailcom>
2015-07-17 12:04:42 -0700

    Moving Elo in a module directory.

--- src/Elo/EloCalculator.php
+++ src/Elo/EloCalculator.php
@@ -1,5 +1,7 @@
 <?php
-namespace pdt256\skill;
+namespace pdt256\skill\Elo;
+
+use pdt256\skill\ParticipantInterface;
 
 class EloCalculator extends AbstractEloCalculator
 {
--- tests/Elo/AllEloCalculatorsTest.php
+++ tests/Elo/AllEloCalculatorsTest.php
@@ -1,5 +1,7 @@
 <?php
-namespace pdt256\skill;
+namespace pdt256\skill\Elo;
+
+use pdt256\skill\Participant;
 
 define('WIN', 1);
 define('LOSE', 0);
