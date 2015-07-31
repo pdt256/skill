8af627e2d23b08272af5225161d8fef1d28c8c08
Jamie Isaacs <pdt256@gmailcom>
2015-07-31 11:27:58 -0700

    Adding more tests for duelling elo calculator.

--- tests/Elo/DuellingEloTest.php
+++ tests/Elo/DuellingEloTest.php
@@ -33,30 +33,67 @@ class DuellingEloCalculatorTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(1500, $newRatingParticipant4);
     }
 
-    public function testGetNewRatingsExpertTeamBeatsBeginnerTeam()
+    public function getNewRatingsData()
     {
-        $participant1 = new Participant(2400);
-        $participant2 = new Participant(2500);
+        return [
+             [ // Equal teams Draw
+                [1500, 1500], [1500, 1500], AbstractEloCalculator::DRAW,
+                [1500, 1500], [1500, 1500],
+            ],
+            [ // Expert team beats beginner team
+                [2400, 2500], [1000, 1100], AbstractEloCalculator::WIN,
+                [2400, 2500], [999, 1099],
+            ],
+            [ // Beginner team beats expert team
+                [2400, 2500], [1000, 1100], AbstractEloCalculator::LOSE,
+                [2368, 2468], [1031, 1131],
+            ],
+        ];
+    }
 
-        $team1 = new Team(AbstractEloCalculator::WIN);
-        $team1->addParticipant($participant1);
-        $team1->addParticipant($participant2);
+    /**
+     * @dataProvider getNewRatingsData
+     */
+    public function testGetNewRatings(
+        $team1Ratings,
+        $team2Ratings,
+        $result,
+        $expectedNewRatingTeam1,
+        $expectedNewRatingTeam2
+    ) {
+        if ($result === AbstractEloCalculator::WIN) {
+            $score1 = AbstractEloCalculator::WIN;
+            $score2 = AbstractEloCalculator::LOSE;
+        } elseif ($result === AbstractEloCalculator::LOSE) {
+            $score1 = AbstractEloCalculator::LOSE;
+            $score2 = AbstractEloCalculator::WIN;
+        } else {
+            $score1 = AbstractEloCalculator::DRAW;
+            $score2 = AbstractEloCalculator::DRAW;
+        }
 
-        $participant3 = new Participant(1000);
-        $participant4 = new Participant(1100);
-
-        $team2 = new Team(AbstractEloCalculator::LOSE);
-        $team2->addParticipant($participant3);
-        $team2->addParticipant($participant4);
+        $team1 = $this->getTeamFromParticipantRatings($team1Ratings, $score1);
+        $team2 = $this->getTeamFromParticipantRatings($team2Ratings, $score2);
 
         $calculator = new DuellingEloCalculator(new EloCalculator(32));
         list($newRatingsTeam1, $newRatingsTeam2) = $calculator->getNewRatings($team1, $team2);
-        list($newRatingParticipant1, $newRatingParticipant2) = $newRatingsTeam1;
-        list($newRatingParticipant3, $newRatingParticipant4) = $newRatingsTeam2;
 
-        $this->assertSame(2400, $newRatingParticipant1);
-        $this->assertSame(2500, $newRatingParticipant2);
-        $this->assertSame(999, $newRatingParticipant3);
-        $this->assertSame(1099, $newRatingParticipant4);
+        $this->assertSame($expectedNewRatingTeam1, $newRatingsTeam1);
+        $this->assertSame($expectedNewRatingTeam2, $newRatingsTeam2);
+    }
+
+    /**
+     * @param int[] $participantRatings
+     * @param float $score
+     * @return Team
+     */
+    private function getTeamFromParticipantRatings(array $participantRatings, $score)
+    {
+        $team = new Team($score);
+        foreach ($participantRatings as $participantRating) {
+            $team->addParticipant(new Participant($participantRating));
+        }
+
+        return $team;
     }
 }
